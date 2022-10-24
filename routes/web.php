150ab55c094c7fa65fd6903c62234f3d2c60983b
hasbulahshopari <hasbullahhasbu01@gmail.com>
2022-10-24 13:41:44 +0700

    menyelesaikan vidio 5 #Model,Collection & Controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -29,57 +32,5 @@ Route::get('/about', function () {
 });
 
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Dilan 1990",
-            "slug" => "dilan-1990",
-            "author" => "BUloh",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero iusto et optio fugiat aliquid. Illum atque earum obcaecati, veniam quod facere, cupiditate architecto repellendus laboriosam voluptatem accusantium sunt, unde consequuntur ex! Ea et numquam ipsum eligendi suscipit molestiae voluptates dolorum doloribus provident! Fugit soluta ipsa molestias magnam ea rem amet, nulla veritatis reprehenderit odio pariatur minus, laborum labore maxime? Distinctio expedita ipsum earum quae dolor autem optio. Nam minus accusamus doloribus suscipit. Quam, cum. Excepturi repellat qui nam ex fugit?"
-        ],
-        [
-            "title" => "Milea",
-            "slug" => "milea",
-            "author" => "Hasbu",
-            "body" => "
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam inventore illum magni id ipsum odit quas sunt reiciendis corporis aspernatur, ut distinctio suscipit non at. Fuga dignissimos cumque vel odit perspiciatis quam consequatur fugiat illum maiores? Perspiciatis, delectus esse eveniet assumenda at odio praesentium exercitationem. Eius deserunt laboriosam repellendus! Dolorem dolore ipsum, minima iste quaerat praesentium itaque quidem fugiat fugit consectetur mollitia sequi nisi ea, recusandae impedit eveniet officia accusamus eum veritatis nobis numquam quia saepe eos. Quo voluptatum aliquam veritatis esse placeat illo tempora vel, deserunt delectus, mollitia ab, nobis quod ex sed iste! Eum consequatur accusamus magnam dicta. "
-        ],
-    ];
-
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
-
-// halaman single post
-Route::get('posts/{slug}', function ($slug) {
-    $blog_posts = [
-        [
-            "title" => "Dilan 1990",
-            "slug" => "dilan-1990",
-            "author" => "BUloh",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero iusto et optio fugiat aliquid. Illum atque earum obcaecati, veniam quod facere, cupiditate architecto repellendus laboriosam voluptatem accusantium sunt, unde consequuntur ex! Ea et numquam ipsum eligendi suscipit molestiae voluptates dolorum doloribus provident! Fugit soluta ipsa molestias magnam ea rem amet, nulla veritatis reprehenderit odio pariatur minus, laborum labore maxime? Distinctio expedita ipsum earum quae dolor autem optio. Nam minus accusamus doloribus suscipit. Quam, cum. Excepturi repellat qui nam ex fugit?"
-        ],
-        [
-            "title" => "Milea",
-            "slug" => "milea",
-            "author" => "Hasbu",
-            "body" => "
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam inventore illum magni id ipsum odit quas sunt reiciendis corporis aspernatur, ut distinctio suscipit non at. Fuga dignissimos cumque vel odit perspiciatis quam consequatur fugiat illum maiores? Perspiciatis, delectus esse eveniet assumenda at odio praesentium exercitationem. Eius deserunt laboriosam repellendus! Dolorem dolore ipsum, minima iste quaerat praesentium itaque quidem fugiat fugit consectetur mollitia sequi nisi ea, recusandae impedit eveniet officia accusamus eum veritatis nobis numquam quia saepe eos. Quo voluptatum aliquam veritatis esse placeat illo tempora vel, deserunt delectus, mollitia ab, nobis quod ex sed iste! Eum consequatur accusamus magnam dicta. "
-        ],
-    ];
-
-    $new_post = [];
-
-    foreach ($blog_posts as $post) {
-        if ($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
-
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']);
+Route::get('posts/{slug}',[PostController::class, 'show']);
