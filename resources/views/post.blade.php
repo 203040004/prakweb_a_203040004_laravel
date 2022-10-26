66aee5830abc187fc496f43257e87b05287876fc
hasbulahshopari <hasbullahhasbu01@gmail.com>
2022-10-26 16:27:00 +0700

    menyelesaikan vidio 8 #Post Category & Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,6 +4,9 @@
   <article>
   <h2>{{ $post->title }}</h2>
   <h5>{{ $post["author"] }}</h5>
+
+  <p>By. Hasbulah in <a href="/categories/{{$post->category->slug}}">{{ $post->category->name}}</a> </P>
+
   {!! $post->body !!}
   </article>
 
