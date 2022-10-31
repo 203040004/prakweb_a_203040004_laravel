729086ecaae5410ff63b1fc615817d8e2eac8820
hasbulahshopari <hasbullahhasbu01@gmail.com>
2022-10-31 17:48:01 +0700

    menyelesaikan vidio 13 #Serching & Pagination

--- routes/web.php
+++ routes/web.php
@@ -21,24 +21,23 @@ use App\Models\User;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home",
-        
-        'active' => 'home'
+        'title' => 'Home',
+        'active' => 'home',
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
-        "title" => "About",
+        'title' => 'About',
         'active' => 'about',
-        "name" => "Hasbuloh",
+        'name' => "Hasbuloh",
         "email" => "hasbu@gmail.com",
         "image" => "poto.jpg"
     ]);
 });
 
 
-Route::get('/blog', [PostController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index']);
 Route::get('posts/{post:slug}',[PostController::class, 'show']);
 
 Route::get('/categories', function() {
@@ -49,16 +48,18 @@ Route::get('/categories', function() {
     ]);
 });
 
-Route::get('/categories/{category:slug}', function(Category $category){
-    return view('posts', [
-        'title' => "Post By Category : $category->name",
-        'posts' => $category->posts->load('category', 'author'),
-    ]);
-});
+// Route::get('/categories/{category:slug}', function(Category $category){
+//     return view('posts', [
+//         'title' => "Post By Category : $category->name",
+//         'active' => 'categories',
+//         'posts' => $category->posts->load('category', 'author'),
+//     ]);
+// });
 
-Route::get('/authors/{author:username}', function(User $author) {
-    return view('posts', [
-        'title' => "Post By Author : $author",
-        'posts' => $author->posts->load('category', 'author'),
-    ]);
-});
+// Route::get('/authors/{author:username}', function(User $author) {
+//     return view('posts', [
+//         'title' => "Post By Author : $author",
+//         'active' => 'posts',
+//         'posts' => $author->posts->load('category', 'author'),
+//     ]);
+// });
