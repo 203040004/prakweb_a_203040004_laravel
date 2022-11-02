a9012050e13811acbfac2ea755b26a787d564aa7
hasbulahshopari <hasbullahhasbu01@gmail.com>
2022-11-02 17:30:17 +0700

    menyelesaikan vidio 15 #User Registration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,18 +17,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // User::create([
-        //     'name' => 'Hasbulah',
-        //     'email' => 'hasbulah@mail.com',
-        //     'password' => bcrypt('12345')
-        // ]);
 
-        // User::create([
-        //     'name' => 'Shezan',
-        //     'email' => 'shezan@mail.com',
-        //     'password' => bcrypt('54321')
-        // ]);
-    
     User::factory(3)->create();
 
     Category::create([
@@ -45,41 +34,6 @@ class DatabaseSeeder extends Seeder
     ]);
 
     Post::factory(20)->create();
-   
-    // Post::create([
-    //     'title' => 'Judul Pertama',
-    //     'slug' => 'judul-pertama',
-    //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis minus culpa veritatis provident incidunt facere, sed perspiciatis quis voluptates nam,',
-    //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis minus culpa veritatis provident incidunt facere, sed perspiciatis quis voluptates nam, vel dolor nemo ea eligendi sit sequi repudiandae saepe. Exercitationem distinctio soluta cum corrupti esse earum ipsum eum, iste quo dignissimos sint voluptatem voluptates harum deleniti ex ullam laudantium qui! Sunt, ut! Aperiam dignissimos perspiciatis et odio ad sint reiciendis iste excepturi amet, molestias non, architecto maxime cumque illo deserunt, eos suscipit quibusdam itaque magni enim voluptate ex consectetur iusto. Voluptas modi, quos quas repudiandae dolorem eligendi reiciendis aut quo voluptatem laboriosam ipsum blanditiis aliquam hic exercitationem ad non libero.',
-    //     'category_id' => 1,
-    //     'user_id' => 1
-    // ]);
-
-    // Post::create([
-    //     'title' => 'Judul Ke Dua',
-    //     'slug' => 'judul-ke-dua',
-    //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis minus culpa veritatis provident incidunt facere, sed perspiciatis quis voluptates nam,',
-    //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis minus culpa veritatis provident incidunt facere, sed perspiciatis quis voluptates nam, vel dolor nemo ea eligendi sit sequi repudiandae saepe. Exercitationem distinctio soluta cum corrupti esse earum ipsum eum, iste quo dignissimos sint voluptatem voluptates harum deleniti ex ullam laudantium qui! Sunt, ut! Aperiam dignissimos perspiciatis et odio ad sint reiciendis iste excepturi amet, molestias non, architecto maxime cumque illo deserunt, eos suscipit quibusdam itaque magni enim voluptate ex consectetur iusto. Voluptas modi, quos quas repudiandae dolorem eligendi reiciendis aut quo voluptatem laboriosam ipsum blanditiis aliquam hic exercitationem ad non libero.',
-    //     'category_id' => 1,
-    //     'user_id' => 1
-    // ]);
-
-    // Post::create([
-    //     'title' => 'Judul Ke Tiga',
-    //     'slug' => 'judul-ke-tiga',
-    //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis minus culpa veritatis provident incidunt facere, sed perspiciatis quis voluptates nam,',
-    //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis minus culpa veritatis provident incidunt facere, sed perspiciatis quis voluptates nam, vel dolor nemo ea eligendi sit sequi repudiandae saepe. Exercitationem distinctio soluta cum corrupti esse earum ipsum eum, iste quo dignissimos sint voluptatem voluptates harum deleniti ex ullam laudantium qui! Sunt, ut! Aperiam dignissimos perspiciatis et odio ad sint reiciendis iste excepturi amet, molestias non, architecto maxime cumque illo deserunt, eos suscipit quibusdam itaque magni enim voluptate ex consectetur iusto. Voluptas modi, quos quas repudiandae dolorem eligendi reiciendis aut quo voluptatem laboriosam ipsum blanditiis aliquam hic exercitationem ad non libero.',
-    //     'category_id' => 2,
-    //     'user_id' => 1
-    // ]);
-
-    // Post::create([
-    //     'title' => 'Judul Ke Empat',
-    //     'slug' => 'judul-ke-empat',
-    //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis minus culpa veritatis provident incidunt facere, sed perspiciatis quis voluptates nam,',
-    //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis minus culpa veritatis provident incidunt facere, sed perspiciatis quis voluptates nam, vel dolor nemo ea eligendi sit sequi repudiandae saepe. Exercitationem distinctio soluta cum corrupti esse earum ipsum eum, iste quo dignissimos sint voluptatem voluptates harum deleniti ex ullam laudantium qui! Sunt, ut! Aperiam dignissimos perspiciatis et odio ad sint reiciendis iste excepturi amet, molestias non, architecto maxime cumque illo deserunt, eos suscipit quibusdam itaque magni enim voluptate ex consectetur iusto. Voluptas modi, quos quas repudiandae dolorem eligendi reiciendis aut quo voluptatem laboriosam ipsum blanditiis aliquam hic exercitationem ad non libero.',
-    //     'category_id' => 2,
-    //     'user_id' => 2
-    // ]);
+    
     }
 }
