56cbb497fe1e2210da063e8e0efce50144199ab7
hasbulahshopari <hasbullahhasbu01@gmail.com>
2022-11-23 23:26:19 +0700

    menyelesaikan vidio 21 #Upload image

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
+    User::create([
+            'name' => 'Sopari Tolani',
+            'username' => 'buloh',
+            'email' => 'TolSopari12@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
     User::factory(3)->create();
 
     Category::create([
